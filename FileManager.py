8095b1a691a5384440b91c673fdc30c8cd3f5c84
Faun471 <41022228+Faun471@users.noreply.github.com>
2023-09-07 05:55:40 +0800

    add gitignore

--- FileManager.py
+++ FileManager.py
@@ -1,16 +1,20 @@
 import os
 import shutil
 from send2trash import send2trash
+from File import File
 
 
 class FileManager:
-    def __init__(self, file):
+    def __init__(self, file: File):
         self.file = file
 
     def rename(self, new_name):
         new_path = os.path.join(os.path.dirname(self.file.path), new_name)
+        self.file.name = new_name
         os.rename(self.file.path, new_path)
         self.file.path = new_path
+
+        return self
 
     def send_to_trash(self):
         send2trash(self.file.path)
